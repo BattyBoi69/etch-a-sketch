989ceb65476151ae1ecabc1986fe7cccb5f86a5e
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-05-26 14:40:28 +0100

    Added a button which determines grid size

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let divContainer = document.querySelector("#container");
+generateGrid(16);
 
-let gridSize = 5;
-for (let i = 0; i < gridSize; i++) {
-	let divGridRow = document.createElement("div");
-	divGridRow.classList.add("row");
-	for (let j = 0; j < gridSize; j++) {
-		let divSubGrid = document.createElement("div");
-		//divSubGrid.textContent = (i*gridSize + j + 1).toString();
-		
-		divSubGrid.addEventListener("mouseenter", () => {
-			divSubGrid.style.backgroundColor = "black";
-		});
+function generateGrid(gridSize) {
+	let divContainer = document.querySelector("#container");
+	divContainer.textContent = "";
 
-		divGridRow.appendChild(divSubGrid);
+	for (let i = 0; i < gridSize; i++) {
+		let divGridRow = document.createElement("div");
+		divGridRow.classList.add("row");
+		for (let j = 0; j < gridSize; j++) {
+			let divSubGrid = document.createElement("div");
+
+			divSubGrid.addEventListener("mouseenter", () => {
+				divSubGrid.style.backgroundColor = "black";
+			});
+
+			divGridRow.appendChild(divSubGrid);
+		}
+		divContainer.appendChild(divGridRow);
 	}
-	divContainer.appendChild(divGridRow);
 }
+
+document.querySelector("button").addEventListener("click", () => {
+	let userInputGridSize;
+	do {
+		userInputGridSize = prompt("Please enter precision (positive integer, max 100)") || 16;
+	} while (!(userInputGridSize <= 100 && 
+		userInputGridSize > 0 && 
+		userInputGridSize % 1 === 0));
+
+	generateGrid(userInputGridSize);
+});
