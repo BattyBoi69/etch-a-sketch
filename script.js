f05d023266d597b3a7e7a67b3cd9bfc62b5fe49a
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-05-26 17:04:46 +0100

    Added 2 buttons and completed the functionality of the first: random RGB colour.

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-generateGrid(16);
+let userInputGridSize = 16;
+generateGrid(userInputGridSize);
+
+document.querySelector("#gridPrecision").addEventListener("click", () => {
+	do {
+		userInputGridSize = prompt("Please enter precision (positive integer, max 100)") || 16;
+	} while (!(userInputGridSize <= 100 && 
+		userInputGridSize > 0 && 
+		userInputGridSize % 1 === 0));
+
+	generateGrid(userInputGridSize);
+});
+
+document.querySelectorAll("#options button").forEach((btn) => {
+	btn.addEventListener("click", () => {
+		document.querySelectorAll("#options button").forEach((subLoopBtn) => {
+			if (subLoopBtn === btn) {
+				subLoopBtn.classList.toggle("btn-toggle-on");
+				if (!subLoopBtn.classList.contains("btn-toggle-on")) {
+					subLoopBtn.classList.add("btn-toggle-off");
+				} else {
+					subLoopBtn.classList.remove("btn-toggle-off");
+				}
+			} else {
+				subLoopBtn.classList.remove("btn-toggle-on");
+				subLoopBtn.classList.add("btn-toggle-off");
+			}
+		});
+		generateGrid(userInputGridSize);
+	});
+});
 
 function generateGrid(gridSize) {
 	let divContainer = document.querySelector("#container");
@@ -11,7 +41,7 @@ function generateGrid(gridSize) {
 			let divSubGrid = document.createElement("div");
 
 			divSubGrid.addEventListener("mouseenter", () => {
-				divSubGrid.style.backgroundColor = "black";
+				divSubGrid.style.backgroundColor = pickSubGridColor();
 			});
 
 			divGridRow.appendChild(divSubGrid);
@@ -20,13 +50,20 @@ function generateGrid(gridSize) {
 	}
 }
 
-document.querySelector("button").addEventListener("click", () => {
-	let userInputGridSize;
-	do {
-		userInputGridSize = prompt("Please enter precision (positive integer, max 100)") || 16;
-	} while (!(userInputGridSize <= 100 && 
-		userInputGridSize > 0 && 
-		userInputGridSize % 1 === 0));
+function pickSubGridColor() {
+	let isCR = document.querySelector("#toggleColorRandomize");
+	let isS = document.querySelector("#toggleShading");
+	if (isCR.classList.contains("btn-toggle-on")) {
+		let randomColor = "rgb(";
+		for (let i = 0; i < 3; i++) {
+			randomColor += Math.floor(Math.random() * 256) + ",";
+		}
+		return randomColor.slice(0, -1) + ")";
+	} else if (isS.classList.contains("btn-toggle-on")) {
+		return "blue";
+	} else {
+		return "black";
+	}
+}
 
-	generateGrid(userInputGridSize);
-});
+
